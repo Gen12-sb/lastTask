e6f262d190a5f3a4d17111f5912391257105d33f
Gen12-sb <118854026+Gen12-sb@users.noreply.github.com>
2022-11-24 00:30:42 +0300

    4nom

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QHeaderView, QTableWidget, QTableWidgetItem
 from PyQt5 import uic
 import sqlite3
+
+
 class cofee_table(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.setWindowTitle('Эспрессо')
+        self.setWindowTitle('Капучино')
+        self.pushButton.clicked.connect(self.new)
         self.conor = sqlite3.connect('coffee.sqlite')
         self.cur = self.conor.cursor()
         self.result = self.cur.execute('''SELECT 
@@ -24,6 +27,40 @@ class cofee_table(QMainWindow):
             for j, val in enumerate(elem):
                 a = QTableWidgetItem(str(val))
                 self.tableWidget.setItem(i, j, a)
+
+    def new(self):
+        self.w2 = New()
+        self.w2.show()
+
+
+
+class New(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('addEditCoffeeForm.ui', self)
+        self.setWindowTitle('Новый')
+        self.conor = sqlite3.connect('coffee.sqlite')
+        self.cur = self.conor.cursor()
+        self.pushButton.clicked.connect(self.make)
+
+    def make(self):
+        a1 = self.lineEdit
+        s = self.lineEdit_2
+        a2 = self.cur.execute(f'''SELECT * FROM g_g 
+                    WHERE d_or_d = '{s}' ''').fetchall()[0][0]
+        a3 = self.lineEdit_3
+        a4 = self.lineEdit_4
+        a5 = self.lineEdit_5
+        a6 = self.lineEdit_6
+        self.neww = f'''INSERT INTO coffee
+                    (sort, g_or_g, roast, teasty, price, vol)
+                    VALUES
+                    (?, ?, ?, ?, ?, ?)'''
+        data_tuple = (a1, a2, a3, a4, a5, a6)
+        self.cur.execute(self.neww, data_tuple)
+        self.conor.commit()
+        self.conor.close()
+        self.close()
 
 
 if __name__ == '__main__':
